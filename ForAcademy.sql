c164a67e9683e64e1287ef065df45006feda96a1
Abdelhamid Ait-ayoub <abdelhamidaitayoub@gmail.com>
2021-12-15 12:39:25 +0100

    🗃️ Add the rest of the tables
    - There remains the event table
    - I'm not use one of db script Generator
    - I hard code it from scratch to control it (db script)

--- ForAcademy.sql
+++ ForAcademy.sql
@@ -1,6 +1,100 @@
 CREATE DATABASE IF NOT EXISTS FORACADEMY;
-
 SET foreign_key_checks = 0;
+
+# Role Table
+# Role Table
+DROP Table IF EXISTS Role;
+CREATE TABLE Role (
+    idRole int AUTO_INCREMENT,
+    name varchar(50) NOT NULL,
+    description varchar(255),
+    active bool,
+
+    CONSTRAINT PK_Role PRIMARY KEY (idRole)
+);
+
+DROP Table IF EXISTS Permission;
+CREATE TABLE Permission (
+    idPermission int AUTO_INCREMENT,
+    name varchar(50) NOT NULL,
+    description varchar(255),
+    active bool,
+    CONSTRAINT PK_Permission PRIMARY KEY (idPermission)
+);
+
+
+
+DROP Table IF EXISTS RolePermission;
+CREATE TABLE RolePermission (
+    idRolePermission int AUTO_INCREMENT,
+    idRole int NOT NULL,
+    idPermission int NOT NULL,
+    assignedAt datetime NOT NULL,
+    updatedAt datetime,
+
+    CONSTRAINT PK_User PRIMARY KEY (idRolePermission, idRole, idPermission),
+    CONSTRAINT FK_RolePermissionRole FOREIGN KEY (idRole) REFERENCES Role(idRole),
+    CONSTRAINT FK_RolePermissionPermission FOREIGN KEY (idPermission) REFERENCES Permission(idPermission)
+);
+
+# SSICA table
+DROP Table IF EXISTS SSICA;
+CREATE TABLE SSICA (
+    idSsica int NOT NULL AUTO_INCREMENT,
+    brand varchar(50),
+
+    CONSTRAINT PK_Ssica PRIMARY KEY (idSsica)
+);
+
+# Spesialiter Table
+DROP TABLE IF EXISTS Specialiter;
+CREATE TABLE Specialiter (
+    idSpecialiter int NOT NULL AUTO_INCREMENT,
+    name varchar(50),
+    description varchar(255),
+
+    CONSTRAINT PK_Specialiter PRIMARY KEY (idSpecialiter)
+);
+
+
+# Scan table (UserSsica)
+DROP Table IF EXISTS Scan;
+CREATE TABLE Scan (
+    idScan int NOT NULL Auto_Increment,
+    idSsica int,
+    idUser int,
+    arrivalDate timestamp,
+    goDate timestamp,
+
+    CONSTRAINT PK_Scan PRIMARY KEY (idScan),
+    CONSTRAINT FK_ScanSsica FOREIGN KEY (idSsica) REFERENCES SSICA(idSsica),
+    CONSTRAINT FK_ScanUser FOREIGN KEY (idUser) REFERENCES User(idUser)
+);
+
+
+# Address table
+DROP TABLE IF EXISTS Address;
+CREATE TABLE Address (
+    idAddress int NOT NULL AUTO_INCREMENT,
+    country varchar(50),
+    region varchar(50),
+    city varchar(50),
+    postalCode varchar(50),
+
+    CONSTRAINT PK_Address PRIMARY KEY (idAddress)
+);
+
+# Factory Table
+DROP TABLE IF EXISTS Factory;
+CREATE TABLE Factory (
+    idFactory int NOT NULL AUTO_INCREMENT,
+    name varchar(50),
+    idAddress int,
+
+    CONSTRAINT PK_Factory PRIMARY KEY (idFactory),
+    CONSTRAINT FK_FactoryAddress FOREIGN KEY (idAddress) REFERENCES Address(idAddress)
+);
+
 #  User table
 DROP TABLE IF EXISTS User;
 CREATE TABLE  User (
@@ -12,9 +106,14 @@ CREATE TABLE  User (
     phone varchar(100),
     gander varchar(50),
     dateOfBirth date,
+    idAddress int,
     active bool,
+    idRole int NOT NULL,
 
-   CONSTRAINT PK_User PRIMARY KEY (idUser)
+    CONSTRAINT PK_User PRIMARY KEY (idUser),
+    CONSTRAINT FK_UserRole FOREIGN KEY (idRole) REFERENCES Role(idRole),
+    CONSTRAINT FK_UserAddress FOREIGN KEY (idAddress) REFERENCES Address(idAddress)
+
 );
 
 #  Manager table
@@ -29,11 +128,40 @@ CREATE TABLE  Manager (
 
 #  Manager table
 DROP TABLE IF EXISTS Trainer;
-CREATE TABLE Trainer(
+CREATE TABLE Trainer (
 	idTrainer int NOT NULL,
+	idSpecialiter int,
 
     CONSTRAINT PK_Trainer PRIMARY KEY (idTrainer),
-    CONSTRAINT FK_TrainerUser FOREIGN KEY (idTrainer) REFERENCES User(idUser)
+    CONSTRAINT FK_TrainerUser FOREIGN KEY (idTrainer) REFERENCES User(idUser),
+    CONSTRAINT FK_TrainerSpecialiter FOREIGN KEY (idSpecialiter) REFERENCES Specialiter(idSpecialiter)
+);
+
+# Class table
+DROP TABLE IF EXISTS Class;
+CREATE TABLE Class (
+    idClass int NOT NULL AUTO_INCREMENT,
+    name varchar(50),
+    idTrainer int,
+    idPromotion int,
+    startDay time,
+    endDay time,
+
+    CONSTRAINT PK_Class PRIMARY KEY (idClass),
+    CONSTRAINT FK_ClassTrainer FOREIGN KEY (idTrainer) REFERENCES Trainer(idTrainer),
+    CONSTRAINT FK_ClassPromotion FOREIGN KEY (idPromotion) REFERENCES Promotion(idPromotion)
+);
+
+# Promotion table
+DROP TABLE IF EXISTS Promotion;
+CREATE TABLE Promotion (
+    idPromotion int NOT NULL AUTO_INCREMENT,
+    name varchar(50),
+    year year,
+    startDate timestamp,
+    endDate timestamp,
+
+    CONSTRAINT PK_Class PRIMARY KEY (idPromotion)
 );
 
 #  Admin table
@@ -59,21 +187,13 @@ CREATE TABLE Secretary (
 DROP TABLE IF EXISTS Student;
 CREATE TABLE Student (
 	idStudent int NOT NULL,
+	idSpecialiter int,
+	idClass int,
 
     CONSTRAINT PK_Student PRIMARY KEY (idStudent),
-    CONSTRAINT FK_StudentUser FOREIGN KEY (idStudent) REFERENCES User(idUser)
+    CONSTRAINT FK_StudentUser FOREIGN KEY (idStudent) REFERENCES User(idUser),
+    CONSTRAINT FK_StudentSpecialiter FOREIGN KEY (idSpecialiter) REFERENCES Specialiter(idSpecialiter),
+    CONSTRAINT FK_StudentClass FOREIGN KEY (idClass) REFERENCES Class(idClass)
 );
-
-# Role Table
-# Role_Permission
-# Permission Table
-# Scan
-# SSICA
-# Events
-# Factory
-# address
-# Class
-# Specialiter
-# Promotion
 
 SET foreign_key_checks = 1;

f2b442f47ff92faee5ef394e3d6438499f17b7d6
abdellatif <ouldeabdellatif40@gmail.com>
2021-12-19 23:06:04 +0100

    i create obj for testing the method insert  but i find a small problem
    so the problem is when i insert the obj i get the msg from the console that the obj has been insert but when i go to the DB i don't find it

--- src/main/java/com/foracademy/console/App.java
+++ src/main/java/com/foracademy/console/App.java
@@ -1,4 +1,6 @@
 package com.foracademy.console;
+import com.foracademy.dao.speciality.SpecialiyDaoImplementation;
+import com.foracademy.models.Speciality;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,8 +9,11 @@ import java.util.Properties;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("HELLO FROM CONSOLE APP");
+       // System.out.println("HELLO FROM CONSOLE APP");
+        Speciality speciality1 = new Speciality(1,"PHISIQUE","it is a good specility");
 
+        SpecialiyDaoImplementation example = new SpecialiyDaoImplementation();
+         example.insert(speciality1);
     }
 
 }

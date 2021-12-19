656f9cbf65e22244cecfdbe006032eb7fb297840
abdellatif <ouldeabdellatif40@gmail.com>
2021-12-19 22:57:57 +0100

    add code in methode insert()

--- src/main/java/com/foracademy/dao/speciality/SpecialiyDaoImplementation.java
+++ src/main/java/com/foracademy/dao/speciality/SpecialiyDaoImplementation.java
@@ -1,7 +1,10 @@
 package com.foracademy.dao.speciality;
-
+import com.foracademy.dao.Database;
 import com.foracademy.models.Speciality;
+import javafx.scene.chart.PieChart;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SpecialiyDaoImplementation implements SpecialityDao{
@@ -22,7 +25,32 @@ public class SpecialiyDaoImplementation implements SpecialityDao{
 
     @Override
     public int insert(Speciality speciality) {
-        return 0;
+        try {
+
+            Database connection = new Database();
+            connection.connect();
+            if (speciality.getId()!=0){
+                var sql1 = "UPDATE speciality set description=? and name=? where idSpeciality=?";
+                PreparedStatement statement = connection.connect().prepareStatement(sql1);
+                statement.setString(1, speciality.getDescription());
+                statement.setString(2,speciality.getName());
+                statement.setInt(3,speciality.getId());
+                statement.execute();
+            }else {
+                var sql2 = " INSERT INTO speciality ( name , description ) VALUES ( ? , ?) ";
+                PreparedStatement statement = connection.connect().prepareStatement(sql2);
+                statement.setString(1,speciality.getName());
+                statement.setString(2,speciality.getDescription());
+                statement.execute();
+            }
+
+            System.out.println(speciality.getName()+" "+ " and "+" "+speciality.getDescription()+" add to database");
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("unable to save ");
+        }
+        return 1;
     }
 
     @Override

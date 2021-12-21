cc0efe3a423785d2758a8147aff27aa82f91bb54
Abdelhamid Ait-ayoub <abdelhamidaitayoub@gmail.com>
2021-12-21 18:18:22 +0100

    :hankey: Cleaning the code and fix some typos.

--- src/main/java/com/foracademy/services/AddressService.java
+++ src/main/java/com/foracademy/services/AddressService.java
@@ -1,9 +1,3 @@
 package com.foracademy.services;
 
-import com.foracademy.dao.DaoFactory;
-import com.foracademy.dao.address.AddressDao;
-import com.foracademy.models.Address;
-
-import java.io.IOException;
-
 public class AddressService { }

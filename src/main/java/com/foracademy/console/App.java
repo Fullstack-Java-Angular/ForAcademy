1792dc5b3474fa9bfa68f20226180b5956001a06
abdellatif <ouldeabdellatif40@gmail.com>
2021-12-21 14:22:30 +0100

    this file where i tested all methods which are in SpecialiyDaoImplementation.java

--- src/main/java/com/foracademy/console/App.java
+++ src/main/java/com/foracademy/console/App.java
@@ -1,4 +1,6 @@
 package com.foracademy.console;
+import com.foracademy.dao.Dao;
+import com.foracademy.dao.speciality.SpecialityDao;
 import com.foracademy.dao.speciality.SpecialiyDaoImplementation;
 import com.foracademy.models.Speciality;
 
@@ -10,10 +12,20 @@ public class App {
 
     public static void main(String[] args) {
        // System.out.println("HELLO FROM CONSOLE APP");
-        Speciality speciality1 = new Speciality(1,"PHISIQUE","it is a good specility");
+    SpecialityDao spc = new SpecialiyDaoImplementation();
+      /* ******************************** for find by Id *********************************** */
+      //     Speciality X=spc.findById(1);
+      //   System.out.println(X);
 
-        SpecialiyDaoImplementation example = new SpecialiyDaoImplementation();
-         example.insert(speciality1);
+        /* ******************************** for find All *********************************** */
+         //  spc.findAll().forEach(System.out::println);
+
+        /* ******************************** for insert or update *********************************** */
+        // Speciality add = new Speciality(0,"math","it is smart specility" );
+      //  spc.insert(add);
+
+        /* ******************************** for delete *********************************** */
+        //spc.delete(1);
     }
 
 }

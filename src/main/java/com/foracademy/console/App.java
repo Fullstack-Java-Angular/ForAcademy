c775d2157496c2335a8be13be18697fd5da71058
abdellatif <ouldeabdellatif40@gmail.com>
2021-12-22 22:00:43 +0100

    remove code not need

--- src/main/java/com/foracademy/console/App.java
+++ src/main/java/com/foracademy/console/App.java
@@ -1,5 +1,5 @@
 package com.foracademy.console;
-import com.foracademy.dao.Dao;
+//import com.foracademy.dao.Dao;
 import com.foracademy.dao.speciality.SpecialityDao;
 import com.foracademy.dao.speciality.SpecialiyDaoImplementation;
 import com.foracademy.models.Speciality;
@@ -14,33 +14,10 @@ import java.util.Properties;
 
 public class App {
 
-<<<<<<< HEAD
+
     public static void main(String[] args) {
        // System.out.println("HELLO FROM CONSOLE APP");
     SpecialityDao spc = new SpecialiyDaoImplementation();
-      /* ******************************** for find by Id *********************************** */
-      //     Speciality X=spc.findById(1);
-      //   System.out.println(X);
 
-        /* ******************************** for find All *********************************** */
-         //  spc.findAll().forEach(System.out::println);
-
-        /* ******************************** for insert or update *********************************** */
-        // Speciality add = new Speciality(0,"math","it is smart specility" );
-      //  spc.insert(add);
-
-        /* ******************************** for delete *********************************** */
-        //spc.delete(1);
-=======
-    public static void main(String[] args) throws IOException {
-        System.out.println("HELLO FROM CONSOLE APP");
-        AddressDao addressDao = DaoFactory.getInstance().getAddressDao();
-        try {
-            addressDao.delete(23);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
->>>>>>> e377ae69ecf372615d984909da459d64af5d8ea6
-    }
-
+}
 }

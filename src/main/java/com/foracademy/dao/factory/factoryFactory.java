2d054e8c3d14ae4de1a6d84c66dafaac9ef56b65
abdellatif <ouldeabdellatif40@gmail.com>
2022-01-03 16:43:06 +0100

    add this file

--- src/main/java/com/foracademy/dao/factory/factoryFactory.java
+++ src/main/java/com/foracademy/dao/factory/factoryFactory.java
@@ -1,2 +1,20 @@
-package com.foracademy.dao.factory;public class factoryFactory {
+package com.foracademy.dao.factory;
+
+import com.foracademy.models.Factory;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class factoryFactory {
+    private  factoryFactory(){}
+
+    public static Factory create(ResultSet resultSet)throws SQLException{
+        Factory factory = new Factory();
+
+        factory.setId(resultSet.getInt("idFactory"));
+        factory.setName(resultSet.getString("name"));
+        factory.setAddress(resultSet.getInt("idAddress"));
+        return factory;
+
+    }
 }

368ea2e4df835c4b7fca4c9ae8969c771fb1d28d
doha <dohaferiadi@gmail.com>
2022-01-04 16:10:27 +0100

    - adding implementations into ScanDao.java

--- src/main/java/com/foracademy/dao/scan/ScanDao.java
+++ src/main/java/com/foracademy/dao/scan/ScanDao.java
@@ -3,4 +3,31 @@ package com.foracademy.dao.scan;
 import com.foracademy.dao.IDao;
 import com.foracademy.models.Scan;
 
-public interface ScanDao extends IDao<Scan> { }
+import java.util.List;
+
+public class ScanDao implements IDao<Scan> {
+    @Override
+    public int delete(int id) {
+        return 0;
+    }
+
+    @Override
+    public List<Scan> findAll() {
+        return null;
+    }
+
+    @Override
+    public Scan findById(int id) {
+        return null;
+    }
+
+    @Override
+    public int insert(Scan scan) {
+        return 0;
+    }
+
+    @Override
+    public int update(Scan scan) {
+        return 0;
+    }
+}
